ae1671caa8d2a873a59f643b4dcb6265482f647a
rehmanpervez778-spec <rehmanpervez778@gmail.com>
2026-03-16 12:27:46 +0500

    Add files via upload

---
+++ jaxvascript-loops/index.js
@@ -0,0 +1,40 @@
+for (let i = 1 ; i <= 20 ; i++) {
+    // console.log("Number" , i);
+}
+
+for (let i = 2 ; i <= 20 ; i += 2) {
+        // console.log("Number" , i);
+    
+}
+
+for (let i = 10; i >= 1 ; i--) {
+    // console.log(i);
+}
+
+ let sum = 0 ;
+
+for(let i = 1 ; i <= 50 ; i++) {
+    sum = sum + i ;
+
+//  console.log(sum , i);
+}
+
+for(let i = 1 ; i <= 10 ; i++){
+    // console.log("7x" + i + "=" + (7 * i));
+    
+}
+
+let string = "JavaScript";
+
+for(let i = 0 ; i < string.length ; i++){
+    console.log(string[i]);
+}
+
+let emoji = prompt("Enter emoji");
+let num = prompt("Enter number");
+
+for(let i = 1 ; i <= num ; i++){
+        console.log(emoji);   
+}
+
+    
